a25f3688d2f86c9f2c4dd68f9dc4ece0f5b2677d
Mehmet Tüken <mehmetuken@hotmail.com>
2022-12-01 15:58:50 +0300

    add comment

--- jobs/index.js
+++ jobs/index.js
@@ -2,6 +2,7 @@ import checkPollsJob from "./check-polls-job.js";
 import chainMaintainersJob from "./chain-maintainers-job.js";
 import setValidatorsJob from "./set-validators-job.js";
 
+// setup all jobs
 export function setupJobs() {
     checkPollsJob();
     chainMaintainersJob();
